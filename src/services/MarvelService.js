314e41bcd6e5ef172e0ca4f04542d63f776d5f71
trix7xter <golyakin.eugene@gmail.com>
2023-12-18 22:27:53 +0300

    add error check, add spinner

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -14,12 +14,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?${this._apiKey}`);
+    getAllCharacters = async() => {
+        const res = this.getResource(`${this._apiBase}characters?${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async(id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (res) => {
+        return {
+            name: res.name,
+            description: res.description,
+            thumbnail: res.thumbnail.path + '.' + res.thumbnail.extension,
+            homepage: res.urls[0].url,
+            wiki: res.urls[1].url
+        }
     }
 }
 
